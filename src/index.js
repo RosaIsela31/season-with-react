907c8ff61b3984c0211614193ea584ae59ca4dbb
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2019-12-26 13:42:24 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position)
+  );
+
+  return <div>Hi there</div>
+}
+ 
+
+ReactDOM.render(<App />, document.getElementById('root'));
